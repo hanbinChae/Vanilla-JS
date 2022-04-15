f9d10635d53fba7c0b2086d21791b4da868f5958
hanbinChae <hanbin2043@gmail.com>
2022-04-15 17:00:50 +0900

    4/15

--- app.js
+++ app.js
@@ -1,36 +1,14 @@
-const title = document.querySelector('.hello h1')
+const h1 = document.querySelector('div.hello:first-child h1')
 
 function handleTitleClick(){
-    title.innerText = 'Grap me!!'
-    console.log('title was clicked');
-    title.style.color='blue';
+    const currentColor = h1.style.color;
+    let newColor;
+    if (currentColor == 'blue'){
+        newColor = 'tomato'
+    }else{
+        newColor ='blue'
+    }
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter(){
-    title.innerText = 'Mouse is here!!';
-}
-
-function handleMouseLeave(){
-    title.innerText = 'Mouse is gone!!';
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = 'tomato';
-}
-function handleWindowCopy(){
-    alert('copier!')
-}
-function handleWindowOffline(){
-    alert('SOS no Wifi');
-}
-function handleWindowOnline(){
-    alert('all GOODD!!')
-}
-title.onclick = handleTitleClick;
-title.addEventListener('mouseenter',handleMouseEnter);
-title.addEventListener('mouseleave',handleMouseLeave)
-
-window.addEventListener("resize",handleWindowResize);
-window.addEventListener("copy",handleWindowCopy);
-window.addEventListener('offline',handleWindowOffline);
-window.addEventListener('online',handleWindowOnline);
+h1.addEventListener('click',handleTitleClick)
