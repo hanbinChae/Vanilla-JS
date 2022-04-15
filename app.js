8c01ea4749478435d4dab37d3e7fbfbe5b217886
hanbinChae <hanbin2043@gmail.com>
2022-04-15 20:29:38 +0900

    4/15/2

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const h1 = document.querySelector('div.hello:first-child h1')
 
 function handleTitleClick(){
-    const currentColor = h1.style.color;
-    let newColor;
-    if (currentColor == 'blue'){
-        newColor = 'tomato'
-    }else{
-        newColor ='blue'
-    }
-    h1.style.color = newColor;
+    const clickClass = 'clicked bye';
+  if(h1.className==clickClass){
+    h1.className="";
+  }else{
+      h1.className=clickClass;
+  }
 }
 
 h1.addEventListener('click',handleTitleClick)
