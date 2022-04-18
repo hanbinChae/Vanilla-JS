0d9f44f8fc39c0f4f018bd08fb31166e90cac982
hanbinChae <hanbin2043@gmail.com>
2022-04-18 14:41:18 +0900

    4/18

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
+const greeting = document.querySelector('#greeting');
+const HIDDEN_CLASS = 'hidden';
 
 function onLoginSubmit(event){
-    event.preventDefault();
+    
+    event.preventDefault(); //HTML 기본 속성 작동하지 않도록 막기
     const username = loginInput.value;
-    console.log(username)
-}
-function handleLinkClick(even){
-    even.preventDefault();
-    console.dir(even);
-    alert("clicked!");
+    localStorage.setItem("username",username);
+    loginForm.classList.add(HIDDEN_CLASS)
+    greeting.innerHTML=`Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASS);
 }
 loginForm.addEventListener('submit',onLoginSubmit);
-link.addEventListener('click',handleLinkClick);
