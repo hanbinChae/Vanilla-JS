c1b7b2bd9f1903b4667fb94ebcbcb1b0cc21f304
hanbinChae <hanbin2043@gmail.com>
2022-04-20 12:35:07 +0900

    4/20

--- JSChrome/js/clock.js
+++ JSChrome/js/clock.js
@@ -1,7 +1,12 @@
-const clock = document.querySelector('h2#clock');
+ 
 
-function sayHello(){
-    console.log('hello');
+function getClock(){
+    const date = new Date();
+    const hour = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = (`${hour}:${minutes}:${seconds}`)
 }
 
-setInterval(sayHello, 5000);
+getClock();
+setInterval(getClock,1000);
